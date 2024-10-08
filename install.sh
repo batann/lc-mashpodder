3f38b598e76256b55fbce79d5803000a9b0f74fd
batann <122584289+batann@users.noreply.github.com>
2024-10-09 09:34:35 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 clear
+BASE_DIR="/home/batan/.config/mashpodder"
+opt=(lc-get-podcasts
+lc-add-subscription
+lc-sample-wrapper-for-cron.sh
+mp.conf
+parse_enclosure.xsl
+sample-wrapper-for-cron.sh)
+
+
+
+
+if [[ ! -d /home/batan/.config/mashpodder ]] ; then
+mkdir -p  /home/batan/.config/mashpodder/podcasts
+fi
+cp -r * /home/batan/.config/mashpodder/
+
 echo -e "\033[34mFiles are configured to work from a Base-Directory on \033[31m/media/batan/mashpodder/\033[0m"
 echo -e "Nothing to install, but LC-YAD-PODCAST is still to be made functional"
 

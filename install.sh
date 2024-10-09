a941e53a829f5ad1622e07ee5cd7cce378e344e2
batann <122584289+batann@users.noreply.github.com>
2024-10-09 06:19:41 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 clear
-BASE_DIR="/home/batan/.config/mashpodder"
-opt=(lc-get-podcasts
-lc-add-subscription
-lc-sample-wrapper-for-cron.sh
-mp.conf
-parse_enclosure.xsl
-sample-wrapper-for-cron.sh)
+BASE_DIR="/home/batan/.config/lc-mashpodder"
 
+if [[ ! -d "/home/batan/.config/lc-mashpodder" ]]; then
+    mkdir -p "$BASE_DIR/podcasts"
+    mv /home/batan/lc-mashpodder/* $BASE_DIR 
 
-
-
-if [[ ! -d /home/batan/.config/mashpodder ]] ; then
-mkdir -p  /home/batan/.config/mashpodder/podcasts
+    echo -e "\033[33m       Following files have been moved to \033[31m$BASE_DIR \033[37m"
+    echo -e "\033[31m- \033[37mlc-get-podcasts"
+    echo -e "\033[31m- \033[37mlc-sample-wrapper-for-cron.sh"
+    echo -e "\033[31m- \033[37mmp.conf"
+    echo -e "\033[31m- \033[37mparse_enclosure.xsl"
+    echo -e "\033[31m- \033[37myad-add-podcast.sh"
+    echo -e "\033[33m        as well as the directory: \033[31m"
+    echo -e "\033[31m- \033[37mpodcasts\033[0m"
 fi
-cp -r * /home/batan/.config/mashpodder/
 
-echo -e "\033[34mFiles are configured to work from a Base-Directory on \033[31m/media/batan/mashpodder/\033[0m"
-echo -e "Nothing to install, but LC-YAD-PODCAST is still to be made functional"
+echo -e "\033[34mThe Base-Directory on \033[31m/home/batan/.config/lc-mashpodder/\033[34mexists"
+
 
